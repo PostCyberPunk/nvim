84c59167cca46b7a33bfd1ac156a0a9b485bf0ad
PostCyberPunk <lyhguest2@outlook.com>
2024-05-02 01:38:32 +0800

    fix:dap

--- lua/plugins/dap/core.lua
+++ lua/plugins/dap/core.lua
@@ -1,12 +1,16 @@
 if require("pcp.extra").imports.dap then
   return {
-    "mfussenegger/nvim-dap",
+    {
+      "nvim-neotest/nvim-nio",
+    },
+    {
+      "mfussenegger/nvim-dap",
 
-    dependencies = {
+      dependencies = {
 
-      -- fancy UI for the debugger
-      {
-        "rcarriga/nvim-dap-ui",
+        -- fancy UI for the debugger
+        {
+          "rcarriga/nvim-dap-ui",
       -- stylua: ignore
       keys = {
         { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
@@ -14,67 +18,69 @@ if require("pcp.extra").imports.dap then
     		{ "<leader>dW", function() require("dapui").float_element('watches',{enter=true,width=60,height=30}) end, desc = "Dap UI" },
     		{ "<leader>dw", function() require("dapui").elements.watches.add() end, desc = "Dap UI",mode={"n","v"}},
       },
-        opts = {
-          icons = {
-            collapsed = "+",
-          },
-					-- stylua :ignore
-          layouts = { { elements = {
-								{ id = "scopes", size = 0.25, },
-                { id = "breakpoints", size = 0.25, },
-                { id = "stacks", size = 0.25, },
-                { id = "watches", size = 0.25, },
+          opts = {
+            icons = {
+              collapsed = "+",
+            },
+            -- stylua :ignore
+            layouts = {
+              {
+                elements = {
+                  { id = "scopes", size = 0.25 },
+                  { id = "breakpoints", size = 0.25 },
+                  { id = "stacks", size = 0.25 },
+                  { id = "watches", size = 0.25 },
+                },
+                position = "left",
+                size = 40,
               },
-              position = "left",
-              size = 40,
+            },
+          },
+          config = function(_, opts)
+            -- setup dap config by VsCode launch.json file
+            -- require("dap.ext.vscode").load_launchjs()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup(opts)
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+              dapui.open({})
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+              dapui.close({})
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+              dapui.close({})
+            end
+          end,
+        },
+
+        -- virtual text for the debugger
+        {
+          "theHamsta/nvim-dap-virtual-text",
+          opts = {},
+        },
+        -- mason.nvim integration
+        {
+          "jay-babu/mason-nvim-dap.nvim",
+          dependencies = "mason.nvim",
+          cmd = { "DapInstall", "DapUninstall" },
+          opts = {
+            -- Makes a best effort to setup the various debuggers with
+            -- reasonable debug configurations
+            automatic_installation = true,
+
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {},
+
+            -- You'll need to check that you have the required things installed
+            -- online, please don't ask me how to install them :)
+            ensure_installed = {
+              -- Update this to ensure that you have the debuggers for the langs you want
             },
           },
         },
-        config = function(_, opts)
-          -- setup dap config by VsCode launch.json file
-          -- require("dap.ext.vscode").load_launchjs()
-          local dap = require("dap")
-          local dapui = require("dapui")
-          dapui.setup(opts)
-          dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open({})
-          end
-          dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close({})
-          end
-          dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close({})
-          end
-        end,
       },
-
-      -- virtual text for the debugger
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {},
-      },
-      -- mason.nvim integration
-      {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = "mason.nvim",
-        cmd = { "DapInstall", "DapUninstall" },
-        opts = {
-          -- Makes a best effort to setup the various debuggers with
-          -- reasonable debug configurations
-          automatic_installation = true,
-
-          -- You can provide additional configuration to the handlers,
-          -- see mason-nvim-dap README for more information
-          handlers = {},
-
-          -- You'll need to check that you have the required things installed
-          -- online, please don't ask me how to install them :)
-          ensure_installed = {
-            -- Update this to ensure that you have the debuggers for the langs you want
-          },
-        },
-      },
-    },
 
   -- stylua: ignore
   keys = {
@@ -99,18 +105,19 @@ if require("pcp.extra").imports.dap then
     { "<leader>dp", function() require("dap.ui.widgets").preview() end, desc = "preview", },
   },
 
-    config = function()
-      local Config = require("lazyvim.config")
-      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+      config = function()
+        local Config = require("lazyvim.config")
+        vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
-      for name, sign in pairs(Config.icons.dap) do
-        sign = type(sign) == "table" and sign or { sign }
-        vim.fn.sign_define(
-          "Dap" .. name,
-          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-        )
-      end
-    end,
+        for name, sign in pairs(Config.icons.dap) do
+          sign = type(sign) == "table" and sign or { sign }
+          vim.fn.sign_define(
+            "Dap" .. name,
+            { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+          )
+        end
+      end,
+    },
   }
 else
   return {}

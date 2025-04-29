36eb52fa8087e587ae759dd61951aeff2e92f9ec
PostCyberPunk <lyhguest2@outlook.com>
2025-04-29 22:14:45 +0800

    remove project_nvim

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -53,18 +53,18 @@ return {
     },
   },
   -- project_nvim --------------
-  {
-    "ahmedkhalf/project.nvim",
-    opts = {},
-    event = "VeryLazy",
-    config = function(_, opts)
-      require("project_nvim").setup(opts)
-      require("telescope").load_extension("projects")
-    end,
-    keys = {
-      { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
-    },
-  },
+  -- {
+  --   "ahmedkhalf/project.nvim",
+  --   opts = {},
+  --   event = "VeryLazy",
+  --   config = function(_, opts)
+  --     require("project_nvim").setup(opts)
+  --     -- require("telescope").load_extension("projects")
+  --   end,
+  --   keys = {
+  --     { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+  --   },
+  -- },
   -----------welcome alpha-nvim
   {
     "goolord/alpha-nvim",

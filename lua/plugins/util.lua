67af64cca867b9d257e7d6f697ce87c658d12b7a
PostCyberPunk <lyhguest2@outlook.com>
2025-05-01 01:32:11 +0800

    fixup! remove project_nvim

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -52,17 +52,4 @@ return {
       },
     },
   },
-  -- project_nvim --------------
-  -- {
-  --   "ahmedkhalf/project.nvim",
-  --   opts = {},
-  --   event = "VeryLazy",
-  --   config = function(_, opts)
-  --     require("project_nvim").setup(opts)
-  --     -- require("telescope").load_extension("projects")
-  --   end,
-  --   keys = {
-  --     { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
-  --   },
-  -- },
 }

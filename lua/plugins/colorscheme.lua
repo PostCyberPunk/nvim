701c1c8b57b7e8f2285a9c80440f88497e1a2b46
PostCyberPunk <lyhguest2@outlook.com>
2025-05-01 03:53:03 +0800

    style: remove tokyo night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -77,18 +77,18 @@ return {
     },
   },
   -- ---------------tokyonight-----
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      dim_inactive = true,
-      on_colors = function(colors)
-        colors.border = "#565f89"
-        colors.bg_visual = "#2d8fb6"
-      end,
-      styles = "moon",
-    },
-    config = true,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     dim_inactive = true,
+  --     on_colors = function(colors)
+  --       colors.border = "#565f89"
+  --       colors.bg_visual = "#2d8fb6"
+  --     end,
+  --     styles = "moon",
+  --   },
+  --   config = true,
+  -- },
 }
 --#region
 --#region_end

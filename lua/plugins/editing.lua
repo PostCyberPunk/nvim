8a538924ebd38840d8c36fe872eb44b6998c37ae
PostCyberPunk <lyhguest2@outlook.com>
2025-05-01 03:26:16 +0800

    fix:hide foldcolumn

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -51,12 +51,12 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
     config = function(_, opts)
-      vim.o.foldcolumn = "1" -- '0' is not bad
+      -- vim.o.foldcolumn = "1" -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
       vim.o.fillchars = [[eob: ,fold: ,foldopen:▼,foldsep: ,foldclose:>]]
-      vim.o.foldcolumn = "1"
+      -- vim.o.foldcolumn = "1"
       --
       opts.provider_selector = function(bufnr, filetype, buftype)
         return { "treesitter", "indent" }

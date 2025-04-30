c951a618c2fc7a611968d9a4917e0116c6a199b7
PostCyberPunk <lyhguest2@outlook.com>
2025-04-30 23:41:36 +0800

    add dashboard recent

--- lua/plugins/ui-snacks.lua
+++ lua/plugins/ui-snacks.lua
@@ -27,7 +27,7 @@ return {
 
  ▄█ ▄█  ▓▄   ▐█ ▓▌  █████  ▄█▓▓▄  █▄ █▄
 ▀▓▓▀██  ██▀  ▐█ █▌  █▓▓██  █████  ██▀▓▓▀
-  ▀  ▀  ▀     ▀ ▀   ▀▀▀▀▀   ▀▀▀   ▀  ▀
+ ▀  ▀  ▀     ▀ ▀   ▀▀▀▀▀   ▀▀▀   ▀  ▀
 ]],
         -- stylua: ignore
         ---@type snacks.dashboard.Item[]
@@ -43,6 +43,27 @@ return {
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
+        },
+        sections = {
+          { section = "header", padding = 0 },
+          { section = "startup", padding = 1 },
+          { pane = 2, icon = " ", section = "keys", padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
         },
       }
       opts.indent = { enabled = false }

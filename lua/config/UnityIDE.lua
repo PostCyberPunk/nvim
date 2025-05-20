979d4c3ab2de40b38e4635118706ac749f44d002
PostCyberPunk <lyhguest2@outlook.com>
2025-05-20 20:50:34 +0800

    feat(unity): update with nix

--- lua/config/UnityIDE.lua
+++ lua/config/UnityIDE.lua
@@ -20,5 +20,16 @@ vim.api.nvim_create_autocmd("FileType", {
     end, { desc = "use hyprctl to focus this nvim" })
 
     vim.keymap.set("n", "<leader>US", "<cmd>UnityStart<cr>", { desc = "Start Unity IDE server" })
+    local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+    local omni = require("omnisharp_extended")
+      -- stylua: ignore
+      vim.list_extend(Keys, {
+        { "gd", function() omni.lsp_definitions() end, desc = "Goto Definition", has = "definition" },
+        { "gr", function() omni.lsp_references() end, nowait = true, desc = "References" },
+        { "gI", function() omni.lsp_implementations() end, desc = "Goto Implementation" },
+        { "gy", function() omni.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+        -- { "<leader>ss", function() Snacks.picker.lsp_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Symbols", has = "documentSymbol" },
+        -- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Workspace Symbols", has = "workspace/symbols" },
+      })
   end,
 })

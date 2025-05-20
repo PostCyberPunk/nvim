8289eeedc740145c09674c1adef8396010f7c699
PostCyberPunk <lyhguest2@outlook.com>
2025-05-20 21:39:25 +0800

    build(flake):ominisharp update with nix

--- lua/plugins/unity/omnisharp.lua
+++ lua/plugins/unity/omnisharp.lua
@@ -1,62 +1,56 @@
-if require("pcp.extra").imports.unity then
-  return {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          omnisharp = {
-            handlers = {
-              ["textDocument/definition"] = function(...)
-                return require("omnisharp_extended").handler(...)
-              end,
-            },
-            keys = {
-              {
-                "gd",
-                function()
-                  Snacks.picker.lsp_definitions()
-                end,
-                desc = "Goto Definition",
-              },
-            },
-            enable_roslyn_analyzers = true,
-            enable_import_completion = true,
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, _)
+      --https://nix-community.github.io/nixvim/plugins/lsp/servers/omnisharp/index.html
+      require("lspconfig").omnisharp.setup({
+        cmd = { "dotnet", vim.fn.getenv("OMNISHARP_PATH") },
+        settings = {
+          FormattingOptions = {
+            EnableEditorConfigSupport = true,
+            OrganizeImports = nil,
+          },
+          RoslynExtensionsOptions = {
+            EnableAnalyzersSupport = true,
+            EnableImportCompletion = nil,
+            AnalyzeOpenDocumentsOnly = true,
+          },
+          Sdk = {
+            IncludePrereleases = true,
           },
         },
-      },
-    },
-    -------------Misc-------------
-    -- {
-    --   "williamboman/mason.nvim",
-    --   opts = function(_, opts)
-    --     opts.ensure_installed = opts.ensure_installed or {}
-    --     vim.list_extend(opts.ensure_installed, { "csharpier", "omnisharp" })
-    --   end,
-    -- },
-    { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        if type(opts.ensure_installed) == "table" then
-          vim.list_extend(opts.ensure_installed, { "c_sharp" })
-        end
-      end,
-    },
-    -- {
-    --   "stevearc/conform.nvim",
-    --   opts = {
-    --     formatters_by_ft = {
-    --       cs = { "csharpier" },
-    --     },
-    --     formatters = {
-    --       csharpier = {
-    --         command = "dotnet-csharpier",
-    --         args = { "--write-stdout" },
-    --       },
-    --     },
-    --   },
-    -- },
-  }
-else
-  return {}
-end
+      })
+    end,
+  },
+  { "Hoffs/omnisharp-extended-lsp.nvim", lazy = (vim.g.isnix ~= 1) },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "c_sharp" })
+      end
+    end,
+  },
+  -------------Misc-------------
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     vim.list_extend(opts.ensure_installed, { "csharpier", "omnisharp" })
+  --   end,
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       cs = { "csharpier" },
+  --     },
+  --     formatters = {
+  --       csharpier = {
+  --         command = "dotnet-csharpier",
+  --         args = { "--write-stdout" },
+  --       },
+  --     },
+  --   },
+  -- },
+}

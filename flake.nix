f73457893d2afb35abc9ba697daafb44868485c5
PostCyberPunk <lyhguest2@outlook.com>
2025-05-24 20:35:01 +0800

    BREAKING CHANGE: !extra redo!
    remove flake-parts
    
    create multiple pacakage
    
    c
    
    c
    
    c
    
    c
    
    c
    
    c
    
    c
    
    c
    
    refactor plugin

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     # neovim-nightly.url = "github:neovim/neovim?dir=contrib";
     # neovim-nightly.inputs.nixpkgs.follows = "nixpkgs";
     evergarden-nvim = {
@@ -38,69 +36,37 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-parts,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-
-      perSystem = {
-        lib,
-        system,
-        ...
-      }: let
-        # Derivation containing all plugins
-        pkgs = import (inputs.nixpkgs) {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        pluginPath = import ./plugins.nix {inherit pkgs lib inputs;};
-        # Derivation containing all runtime dependencies
-        runtimePath = import ./runtime.nix {inherit pkgs;};
-
-        # Link together all treesitter grammars into single derivation
-        treesitterPath = pkgs.symlinkJoin {
-          name = "lazyvim-nix-treesitter-parsers";
-          paths =
-            pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
-        };
-
-        # Use nightly neovim only ;)
-        # neovimNightly = inputs.neovim-nightly.packages.${system}.default;
-        # Wrap neovim with custom init and plugins
-        lua = pkgs.lua;
-        neovimWrapped = pkgs.wrapNeovim pkgs.neovim-unwrapped {
-          configure = {
-            customRC =
-              # vim
-              ''
-                " Populate paths to neovim
-                " let g:config_path = "${./.}"
-                let g:plugin_path = "${pluginPath}"
-                let g:runtime_path = "${runtimePath}"
-                let g:treesitter_path = "${treesitterPath}"
-                let g:isnix = 1
-                " Begin initialization
-                source ${./init.lua}
-              '';
-            packages.all.start = [pkgs.vimPlugins.lazy-nvim];
-          };
-          extraLuaPackages = ps: with ps; [lua-utils-nvim pathlib-nvim];
-        };
-      in {
-        packages = rec {
-          # Wrap neovim again to make runtime dependencies available
-          nvim = pkgs.writeShellApplication {
-            name = "nvim";
-            runtimeInputs = [runtimePath];
-            runtimeEnv = {OMNISHARP_PATH = "${pkgs.omnisharp-roslyn}/lib/omnisharp-roslyn/OmniSharp.dll";};
-            text = ''${neovimWrapped}/bin/nvim "$@"'';
-          };
-          default = nvim;
-        };
+  outputs = {nixpkgs, ...} @ inputs: let
+    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems f;
+    mkPkg = system: let
+      inherit system;
+      pkgs = import nixpkgs {
+        inherit system;
+        #NOTE:Some AI plugin is marked as unfree
+        config.allowUnfree = true;
+        overlays =
+          inputs.overlays or [
+            (self: super: {
+              pcp_nvim = {
+                useXDG = false;
+                extraArgs = [];
+              };
+            })
+          ];
       };
+      lib = pkgs.lib;
+      nvim = import ./nix {inherit pkgs lib inputs;};
+    in {
+      default = nvim {};
+      full = nvim {
+        useXDG = true;
+        extraArgs = ["dap" "cpp" "rust" "unity" "ai" "neorg" "extraTheme"];
+      };
+      extra = nvim pkgs.pcp_nvim;
     };
+  in {
+    packages = forAllSystems mkPkg;
+    modules = import ./nix/module.nix;
+  };
 }

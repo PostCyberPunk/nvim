cba663df70dcb3cc8c0bf7e8a548b5455088affd
PostCyberPunk <lyhguest2@outlook.com>
2025-05-25 17:10:17 +0800

    Revert "!feat(project)use persistense"
    This reverts commit 7076431ce34139d5a8ba7b52423b073d1ffff3bb.

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,20 +1,51 @@
 return {
   {
     "folke/persistence.nvim",
+    enabled = false,
+  },
+  {
+    "olimorris/persisted.nvim",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+    config = function()
+      require("persisted").setup({
+        autosave = true,
+      })
+    end,
     keys = {
       {
         "<leader>qp",
+        "<cmd>SessionSelect<cr>",
+        desc = "Search Sessions",
+      },
+      {
+        "<leader>qs",
         function()
-          Snacks.picker.projects()
+          require("persisted").load()
         end,
+        desc = "Restore Session",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persisted").load({ last = true })
+        end,
+        desc = "Restore Last Session",
+      },
+      {
+        "<leader>qD",
+        function()
+          require("persisted").stop()
+        end,
+        desc = "Don't Save Current Session",
       },
       {
         "<leader>qd",
         function()
-          require("persistence").save()
-          require("persistence").stop()
+          vim.cmd("SessionSave")
+          vim.cmd("SessionStop")
           vim.cmd("%bd")
           Snacks.dashboard.open()
+          vim.cmd("bd#")
         end,
         desc = "Quit all and open dashboard",
       },

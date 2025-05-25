e6e4b1c6e2209361393a918ae801f2bb6fd207b7
PostCyberPunk <lyhguest2@outlook.com>
2025-05-26 04:18:21 +0800

    build(nix-modules):stripped from nixpkgs
    etc for xdg

--- nix/module.nix
+++ nix/module.nix
@@ -6,10 +6,10 @@ inputs: {
 }:
 with lib; let
   inherit (pkgs.stdenv.hostPlatform) system;
-  cfg = config.pcp-nvim;
-  package = inputs.self.packages.${system}.nvim {inherit (cfg) useXDG extraPlugins;};
+  cfg = config.programs.pcp-nvim;
+  package = inputs.self.packages.${system}.nvim;
 in {
-  options.pcp-nvim = with types; {
+  options.programs.pcp-nvim = with types; {
     enable = mkEnableOption "Enable my custom nvim setup.";
     useXDG = mkEnableOption "Enable my custom nvim setup.";
     extraPlugins = mkOption {
@@ -18,11 +18,35 @@ in {
       default = [];
       example = ["dap" "cpp" "rust" "unity" "java" "ai" "neorg" "extraTheme"];
     };
+    # from nixpkgs
+    defaultEditor = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        When enabled, installs neovim and configures neovim to be the default editor
+        using the EDITOR environment variable.
+      '';
+    };
+    viAlias = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Symlink {command}`vi` to {command}`nvim` binary.
+      '';
+    };
+    vimAlias = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Symlink {command}`vim` to {command}`nvim` binary.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
+    environment.variables.EDITOR = lib.mkIf cfg.defaultEditor (lib.mkOverride 900 "nvim");
     environment.systemPackages = [
-      package
+      (package {inherit (cfg) useXDG extraPlugins vimAlias viAlias;})
     ];
   };
 }
